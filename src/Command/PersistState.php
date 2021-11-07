f6ad1e7227bf7e1e51f214b34d218724544db039
GitHub <action@github.com>
2021-11-07 15:14:09 +0000

    Merge pull request #32 from hasura-extra/feat/metadata

---
+++ src/Command/PersistState.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * (c) Minh Vuong <vuongxuongminh@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace Hasura\Metadata\Command;
+
+use Hasura\Metadata\StateProcessorInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class PersistState extends Command
+{
+    public function __construct(private iterable $processors)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $symfonyStyle = new SymfonyStyle($input, $output);
+
+        $symfonyStyle->section(
+            sprintf('Persisting application state to Hasura...')
+        );
+
+        foreach ($this->processors as $processor) {
+            /** @var StateProcessorInterface $processor */
+            $processor->process();
+        }
+
+        $symfonyStyle->success('Congratulation! Application state persisted with Hasura!');
+
+        return 0;
+    }
+}
---
+++ tests/Command/PersistStateTest.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * (c) Minh Vuong <vuongxuongminh@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace Hasura\Metadata\Tests\Command;
+
+use Hasura\Metadata\Command\PersistState;
+use Hasura\Metadata\StateProcessorInterface;
+use Hasura\Metadata\Tests\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class PersistStateTest extends TestCase
+{
+    public function testPersistRemoteSchema(): void
+    {
+        $command = new PersistState([$this->createMockProcessor()]);
+        $tester = new CommandTester($command);
+        $tester->execute([]);
+
+        $this->assertStringContainsString('Persisting application state to Hasura...', $tester->getDisplay());
+        $this->assertStringContainsString('Congratulation! Application state persisted with Hasura!', $tester->getDisplay());
+    }
+
+    private function createMockProcessor(): StateProcessorInterface
+    {
+        $mock = $this->createMock(StateProcessorInterface::class);
+        $mock->expects($this->once())->method('process');
+
+        return $mock;
+    }
+}
